46313827c943e9d98ea0122ffdcdef0a711f3512
shariq <shariq.mohd09@gmail.com>
2020-06-25 14:47:01 +0400

    First commit

---
+++ src/main/java/com/hc/dao/UserDAL.java
@@ -0,0 +1,20 @@
+package com.hc.dao;
+
+import java.util.List;
+
+import com.hc.model.User;
+
+public interface UserDAL {
+
+	List<User> getAllUsers();
+
+	User getUserById(String userId);
+
+	User addNewUser(User user);
+
+	Object getAllUserSettings(String userId);
+
+	String getUserSetting(String userId, String key);
+
+	String addUserSetting(String userId, String key, String value);
+}
---
+++ src/main/java/com/hc/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package com.hc.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hc.model.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
